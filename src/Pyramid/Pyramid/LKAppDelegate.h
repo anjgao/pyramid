c77cd053286a0fe04f8d6ce80bc14c1346ab02e2
andregao <anjgao@gmail.com>
2013-01-10 11:27:58 +0800

    add empty project template

---
+++ src/Pyramid/Pyramid/LKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LKAppDelegate.h
+//  Pyramid
+//
+//  Created by andregao on 13-1-10.
+//  Copyright (c) 2013年 linkkk.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ src/Pyramid/Pyramid/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Pyramid
+//
+//  Created by andregao on 13-1-10.
+//  Copyright (c) 2013年 linkkk.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LKAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LKAppDelegate class]));
+    }
+}
